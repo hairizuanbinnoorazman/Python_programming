6d5dc4af52f6c8fe631092df21ddf5e4764b226f
Hairizuan <hairizuanbinnoorazman@gmail.com>
2024-05-30 01:39:30 +0800

    feat: Added runtime column

--- docker_code_executor/migrate.py
+++ docker_code_executor/migrate.py
@@ -3,7 +3,8 @@ import sqlite3
 con = sqlite3.connect("example.db")
 cur = con.cursor()
 cur.execute("""CREATE TABLE IF NOT EXISTS job(
-    id TEXT PRIMARY KEY, 
+    id TEXT PRIMARY KEY,
+    runtime TEXT NOT NULL,
     status TEXT NOT NULL, 
     date_created TEXT NOT NULL,
     date_modified TEXT NOT NULL)""")
